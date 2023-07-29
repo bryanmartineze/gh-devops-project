c3941d8110ba23eebf4fa1414f5989ac471d3409
bryanmartineze <bryanmartineze@gmail.com>
2023-07-29 00:28:07 +0000

    added terraform directory

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  cloud {
+    organization = "bryanmartineze-devops"
+
+    workspaces {
+      name = "cicd-pipeline-example"
+    }
+  }
+}
+
+variable "aws_region" {
+    
+}
+
+provider "aws" {
+  region = var.aws_region
+}
