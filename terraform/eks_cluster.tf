5fe0695f2d7db4e5e926e02eab1218b891006ec9
bryanmartineze <bryanmartineze@gmail.com>
2023-09-07 23:31:17 +0000

    testing k8s v 1.24

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -4,7 +4,7 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = "trainschedule"
-  cluster_version = "1.25"
+  cluster_version = "1.24"
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access = true
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
